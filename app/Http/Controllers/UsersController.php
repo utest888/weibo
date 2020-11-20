e21bab4582e58007cd83d8f6f3f4810a7e42f94e
unknown <utest888@qq.com>
2020-11-20 10:47:56 +0800

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,10 +22,18 @@ class UsersController extends Controller
     {
         $this->validate($request, [
             'name' => 'required|unique:users|min:3|max:50',
-            'email' => 'required|email|unique:users|max|255',
+            'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
 
-        return;
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        session()->flash('success', "注册成功");
+
+        return redirect()->route('users.show', [$user]);
     }
 }
