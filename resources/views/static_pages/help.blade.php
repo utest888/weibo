67368f1429157cae207e252b88251d4adeb80627
unknown <utest888@qq.com>
2020-11-20 08:59:01 +0800

    基础页面

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title',"帮助")
+
+@section('content')
+帮助
+@stop
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title',"首页")
+
+@section('content')
+主页
+@stop
