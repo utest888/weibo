41d74915542ab7eaacc8fa012fa3ee82ba9ce14c
unknown <utest888@qq.com>
2020-11-20 13:23:38 +0800

    激活用户

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class UsersController extends Controller
 {
@@ -12,12 +13,24 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store', 'index']
+            'except' => ['show', 'create', 'store', 'index', 'confirmEmail']
         ]);
 
         $this->middleware('guest', [
             'only' => ['create']
         ]);
+    }
+
+    public function confirmEmail($token)
+    {
+        $user = User::where('activation_token', $token)->firstOrFail();
+        $user->activated = true;
+        $user->activation_token = null;
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success', '激活成功');
+        return redirect()->route('users.show', [$user]);
     }
 
     public function index()
@@ -51,11 +64,26 @@ class UsersController extends Controller
             'password' => bcrypt($request->password)
         ]);
 
-        Auth::login($user);
+        $this->sendEmailConfirmationTo($user);
 
-        session()->flash('success', "注册成功");
+        session()->flash('success', "验证邮件已发送");
 
-        return redirect()->route('users.show', [$user]);
+        return redirect('/');
+    }
+
+    protected function sendEmailConfirmationTo($user)
+    {
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = 'test@test.com';
+        $name = 'test';
+        $to = $user->email;
+
+        $subject = "请确认你的邮箱";
+
+        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
     }
 
     public function edit(User $user)
@@ -89,7 +117,7 @@ class UsersController extends Controller
     {
 
         $this->authorize('destroy', $user);
-        
+
         $user->delete();
         session()->flash('success', '成功删除用户');
         return back();
