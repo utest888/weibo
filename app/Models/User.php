9f327ebb342aa549df97811643407717e8cd70f7
unknown <utest888@qq.com>
2020-11-20 11:15:57 +0800

    t

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
 
-        return "http://www.gravatar.com/avatar/$hash?s=$size";
+        return "http://www.gravatar.com/avatar/$hash?s=$size"; 
     }
 
     /**
